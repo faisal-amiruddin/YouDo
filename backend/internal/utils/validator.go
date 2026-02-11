72acf72dff266f5d6e6e674ae92e76ca9c8806d0
Faisal Amiruddin <faisalamrdn@gmail.com>
2026-02-11 16:56:20 +0700

    add utils validator

---
+++ backend/internal/utils/validator.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"regexp"
+	"strings"
+)
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+
+func ValidateEmail(email string) bool {
+	return emailRegex.MatchString(email)
+}
+
+func ValidatePassword(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+
+	hasLetter := regexp.MustCompile(`[a-zA-Z]`).MatchString(password)
+	hasNumber := regexp.MustCompile(`[0-9]`).MatchString(password)
+
+	return hasLetter && hasNumber
+}
+
+func SanitizeString(s string) string {
+	return strings.TrimSpace(s)
+}
